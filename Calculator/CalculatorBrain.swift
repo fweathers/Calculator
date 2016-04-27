9c0de03d00f3671342a1ac361637814e9b827c95
fweathers <fel.weathers@gmail.com>
2016-04-26 20:50:29 -0400

    Added CalculatorBrain file for model(MVC)

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+class CalculatorBrain
+{
+    func setOperand(operand: Double) { }
+    
+    func performOperation(symbol: String) { }
+    
+    var result: Double {
+        get {
+            return 0.0
+        }
+    }
+}
